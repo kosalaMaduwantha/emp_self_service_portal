cb141e21359b9ce13d2e5c9c257503a7b133366c
kosalaMaduwantha <52875765+kosalaMaduwantha@users.noreply.github.com>
2022-12-21 22:57:38 +0530

    adding changes

--- Request/views.py
+++ Request/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from Request.models import Request
+from Request.serializer import RequestSerializer
+
+# Create your views here.
+
+
+# request change data request to employee information  
+@api_view(['POST'])
+def employee_request_post(request):
+    
+    if request.method == 'POST':
+        serializer = RequestSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)   
+    
+
+@api_view(['GET'])
+def get_employees_requests(request, emp_key):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        requests = Request.objects.filter(employee=emp_key) 
+        serializer = RequestSerializer(requests, many=True)
+        return Response(serializer.data)
---
+++ employee/serializer.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers 
+from employee.models import Employee
+ 
+ 
+class EmployeeSerializer(serializers.ModelSerializer):
+ 
+    class Meta:
+        model = Employee
+        fields = ('legal_first_name',
+                  'legal_last_name',
+                  'pref_first_name',
+                  'pref_last_name',
+                  'NIC',
+                  'age',
+                  'gender',
+                  'dob',
+                  'country_of_birth',
+                  'marital_status',
+                  'ethnicity',
+                  'nationality',
+                  'citizenship_status',
+                  'department')
+        
+        
+        
+        
+        
