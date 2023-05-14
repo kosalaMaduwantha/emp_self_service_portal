92be3590bd28974f3a734e23cea76c9f91d06c65
kosalaMaduwantha <52875765+kosalaMaduwantha@users.noreply.github.com>
2023-05-14 16:38:37 +0530

    added the serializer to serialize bothe department and employee

--- employee/serializer.py
+++ employee/serializer.py
@@ -1,28 +1,16 @@
 from rest_framework import serializers 
-from employee.models import Employee
+from employee.models import Employee, Department
  
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = '__all__'
  
 class EmployeeSerializer(serializers.ModelSerializer):
- 
+    department = DepartmentSerializer()
     class Meta:
         model = Employee
-        fields = ('legal_first_name',
-                  'legal_last_name',
-                  'pref_first_name',
-                  'pref_last_name',
-                  'NIC',
-                  'age',
-                  'email',
-                  'gender',
-                  'dob',
-                  'country_of_birth',
-                  'marital_status',
-                  'ethnicity',
-                  'nationality',
-                  'citizenship_status',
-                  'department',
-                  'phone',
-                  'address')
+        fields = '__all__'
         
         
         
